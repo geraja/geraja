1a5a3566bc7a506b150ed3533d0972f90f172ea9
Isânio Moraes <isanio@yahoo.com>
2016-04-22 17:49:58 -0300

    Updates home page

--- app/views/pagina-inicial.php
+++ app/views/pagina-inicial.php
@@ -1,2 +1,4 @@
-<h1>Gerador de Jogos</h1>
-<p>Página inicial</p>
+<div class="text-center">
+  <h1>Gerador de Jogos</h1>
+  <p><a class="btn btn-link" href="<?= base_url('jogo/demo'); ?>">Jogo Demonstração</a></p>
+</div>
