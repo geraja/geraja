cf0de449f0834f253443387ed8d5711cdbb2a7c2
Isânio Moraes <isanio@yahoo.com>
2016-05-31 20:50:06 -0300

    Change app session control and add example games on home page

--- app/config/config.php
+++ app/config/config.php
@@ -367,10 +367,10 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'sessions';
+$config['sess_driver'] = 'database';
+$config['sess_cookie_name'] = 'gerajacisessions';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = (ENVIRONMENT === 'production') ? 'sessions' : NULL;
+$config['sess_save_path'] = 'ci_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
@@ -393,7 +393,7 @@ $config['sess_regenerate_destroy'] = FALSE;
 $config['cookie_prefix']	= '';
 $config['cookie_domain']	= '';
 $config['cookie_path']		= '/';
-$config['cookie_secure']	= ENVIRONMENT === 'production') ? true : false;
+$config['cookie_secure']	= FALSE;
 $config['cookie_httponly'] 	= TRUE;
 
 /*
--- app/views/pages/home-page.php
+++ app/views/pages/home-page.php
@@ -18,21 +18,25 @@
   <div class="container-games">
     <div class="row">
       <div class="col-md-3">
-        <div class="game-item">
-          <div class="game-thumb"></div>
-          <h3 class="game-title"><a href="#">Jogo Exemplo</a></h3>
+        <div class="game-item seeing-version">
+          <div class="game-thumb">
+            <img src="<?= base_url('public/images/eye.svg'); ?>" alt="Imagens rápidas" width="60">
+          </div>
+          <h3 class="game-title"><a target="_blank" href="https://geraja.com.br/jogo/1sdt9f9kfo">Imagens rápidas</a></h3>
         </div>
       </div>
       <div class="col-md-3">
-        <div class="game-item">
-          <div class="game-thumb"></div>
-          <h3 class="game-title"><a href="#">Jogo Exemplo</a></h3>
+        <div class="game-item listening-version">
+          <div class="game-thumb">
+            <img src="<?= base_url('public/images/low-vision.svg'); ?>" alt="Sons diversos" width="60">
+          </div>
+          <h3 class="game-title"><a target="_blank" href="https://geraja.com.br/jogo/26vnqxa3a4">Sons diversos</a></h3>
         </div>
       </div>
       <div class="col-md-3">
-        <div class="game-item">
-          <div class="game-thumb"></div>
-          <h3 class="game-title"><a href="#">Jogo Exemplo</a></h3>
+        <div class="game-item seeing-version">
+          <div class="game-thumb"><img src="<?= base_url('public/images/eye.svg'); ?>" alt="Imagens rápidas" width="60"></div>
+          <h3 class="game-title"><a href="https://geraja.com.br/jogo/6jdxrb3cvy">Pensando rápido</a></h3>
         </div>
       </div>
       <div class="col-md-3">
